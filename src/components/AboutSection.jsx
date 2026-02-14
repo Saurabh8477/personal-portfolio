ba187cbc4092825df7f77d46546785e2124104db
Saurabh8477 <saurabhdan8477@gmail.com>
2026-02-14 16:57:05 +0530

    Initial commit

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -35,7 +35,7 @@ export const AboutSection = () => {
               </a>
 
               <a
-                href="saurabhresume.pdf"
+                href="public/projects/saurabhresume.pdf"
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
               >
                 Download CV
